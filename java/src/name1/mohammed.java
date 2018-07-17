edace6db951ed36499ed57f3444c3a3a975ff0c6
ImranAyub786 <mohammedayub@academytrainee.com>
2018-07-17 09:56:37 +0100

    Commit Java

---
+++ java/src/name1/mohammed.java
@@ -0,0 +1,13 @@
+package name1;
+
+public class mohammed {
+	
+	public static void main(String[] args) {
+		String Hello = "HelloMo!!";
+		System.out.println("String Hello");
+		System.out.println("Hellomo!!");
+		System.out.println("HelloWorld!!");
+		System.out.println("Minimum Value = " + Hello);
+	}
+		
+}
---
+++ java/src/name1/parameter.java
@@ -0,0 +1,10 @@
+package name1;
+
+public class parameter {
+
+private int exampleParameter;
+
+private int exampleMethod(int Parameter) {
+	exampleParameter = exampleParameter + 1;
+	return exampleParameter;
+}}
